16c8037885ec034564102cc4c076ab80a3778cb2
user <user@MacBook-Pro.local>
2024-01-11 22:11:21 -0800

    added profile pic impl

--- src/app/components/dashboard/overview/overview.component.ts
+++ src/app/components/dashboard/overview/overview.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ClientsService } from 'src/app/services/clients.service';
 
 @Component({
   selector: 'app-overview',
@@ -12,12 +13,18 @@ export class OverviewComponent {
   showModal = false;
   showUserFormModal = false;
   showTerminalFormModal = false;
-
+  selectedImage: File | any = null;
   myform : FormGroup;
+  profileImageForm: FormGroup;
+  emailAddress: string = "";
+  base64Char : String = "";  
 
   constructor( private http: HttpClient,
+    private clientService: ClientsService,
     private formBuilder: FormBuilder,
     private router: Router){
+
+
       this.myform = new FormGroup({
         uName: new FormControl('',	[Validators.required]),
         mobile: new FormControl('',	[Validators.required,  Validators.pattern(/^\+?\d{1,3}[-\s]?\d{9,11}$/)]),
@@ -27,9 +34,16 @@ export class OverviewComponent {
         optionChecked : new FormControl('',   [Validators.required]),
    
      }); 
+
+      this.profileImageForm = new FormGroup({
+        profileImage: new FormControl( this.base64Char,	[Validators.required]),
+      })
     }
 
   get formData() { return this.myform.controls; };
+
+  get profileImageFormData() { return this.profileImageForm.controls; };
+
 
   toggleModal(){
     this.showModal = !this.showModal;
@@ -44,6 +58,60 @@ export class OverviewComponent {
     this.showModal = !this.showModal;
     this.showTerminalFormModal = !this.showTerminalFormModal;
 
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedImage = event.target.files[0];
+    
+    // Read the selected image as a base64-encoded string
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.profileImageForm.patchValue({
+        profileImage: reader.result as string
+      });
+    };
+    reader.readAsDataURL(this.selectedImage);
+    this.base64Char = this.profileImageForm.get('profileImage')?.value;
+
+  }
+
+  // onSubmitProfileImage(user: any){
+
+  //   console.log("entered the submit btn");
+  //   console.log(this.profileImageForm.value);
+
+  //   this.clientService.updateCustomer(this.profileImageForm.value, this.emailAddress).subscribe({
+  //     next: (response: any) => {
+
+  //       console.log("entered response", response);
+  //     },
+  //     error: (error: any) => {
+  //       console.error("entered error", error);
+  //     }
+  //   });
+  // }
+
+  onSubmitProfileImage(user: any): void {
+
+    console.log("entered the submit btn");
+    this.base64Char = this.profileImageForm.get('profileImage')?.value;
+
+    console.log(this.base64Char);
+
+    console.log(this.profileImageForm.value);
+
+    // Use FormData to send the base64-encoded image to the backend
+    const formData = new FormData();
+    formData.append('profileImage', this.profileImageForm.get('profileImage')?.value);
+
+    this.clientService.updateCustomer(formData, this.emailAddress).subscribe({
+      next: (response: any) => {
+        console.log("entered response", response);
+      },
+      error: (error: any) => {
+        console.error("entered error", error);
+      }
+    });
   }
 
 
--- src/app/services/clients.service.ts
+++ src/app/services/clients.service.ts
@@ -1,5 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// import { FetchResult, InMemoryCache } from '@apollo/client';
+// import { Apollo, gql } from 'apollo-angular';
+// import { HttpLink } from 'apollo-angular-link-http';
+import { Apollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { gql } from 'graphql-tag';
+import { MutationOptions, FetchResult, InMemoryCache } from '@apollo/client/core';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
@@ -10,14 +17,23 @@ export class ClientsService {
   baseURL = "http://localhost:8080";
   singupUrl = 'https://smartb2c.ubagroup.com/bscv2/api/Accounts/Login';
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private apollo: Apollo, private httpLink: HttpLink, private httpClient: HttpClient) {
+    const uri = 'http://localhost:8085/graphql'; 
+    const http = httpLink.create({ uri });
+    apollo.create({
+      link: http,
+      cache: new InMemoryCache(),
+    });
+  }
+
 
   getCountryCode() {
-    return this.http.get<any>('https://ipinfo.io/json');
+    return this.httpClient.get<any>('https://ipinfo.io/json');
   }
 
   // getCountryNameByCode(countryCode: string):Observable<string> {
-  //   return this.http.get<any>(`https://restcountries.com/v2/alpha/${countryCode}`).pipe(
+  //   return this.httpClient.get<any>(`https://restcountries.com/v2/alpha/${countryCode}`).pipe(
   //     map((res: any)=> {
   //       console.log("map",res);
   //       return res.name;
@@ -26,56 +42,89 @@ export class ClientsService {
   // }
 
 
+
+  updateCustomer(customerUpdateRequest: any, emailAddress: string): Observable<FetchResult<any>>{
+
+    console.log("entered the submit service btn");
+
+    const mutation = gql`
+    mutation{
+      updateCustomer(customerRequest: {
+        firstName:"${customerUpdateRequest.firstName}",
+        lastName:"${customerUpdateRequest.lastName}",
+        country:"${customerUpdateRequest.country}",
+        profileImage:"${customerUpdateRequest.profileImage}"
+      }, emailAddress:"${emailAddress}"){
+        customerId
+        firstName
+        lastName
+        emailAddress
+        phoneNumber
+        tier
+        country
+        hasPin
+        enablePush
+        profileImage
+      }
+    }
+    `;
+
+    return this.apollo.mutate<any>({
+      mutation
+    });
+  }
+
+
   getCountryNameByCode(countryCode: string) {
-    return this.http.get<any>(`https://restcountries.com/v2/alpha/${countryCode}`);
+    return this.httpClient.get<any>(`https://restcountries.com/v2/alpha/${countryCode}`);
   }
 
   terminalRequest(userDetails:any){
     console.log("hello world");
     const headers = new HttpHeaders()
     .append('Content-Type', 'application/json')
-    return this.http.post<any>(this.baseURL + "terminal request", userDetails);
+    return this.httpClient.post<any>(this.baseURL + "terminal request", userDetails);
   }
 
   accountLogin(authCredentials:any){
     console.log("hello world");
     const headers = new HttpHeaders()
     .append('Content-Type', 'application/json')
-    return this.http.post<any>(this.singupUrl, authCredentials);
+    return this.httpClient.post<any>(this.singupUrl, authCredentials);
   }
 
   passwordReset(usersDetail:any){
     console.log("hello world");
     const headers = new HttpHeaders()
     .append('Content-Type', 'application/json')
-    return this.http.post<any>(this.singupUrl, usersDetail);
+    return this.httpClient.post<any>(this.singupUrl, usersDetail);
   }
 
   getTerminals(): Observable<any> {
-    return this.http.get<any>('assets/data/orders.json');
+    return this.httpClient.get<any>('assets/data/orders.json');
   }
   
   getTerminal1(): Observable<any> {
-    return this.http.get<any>('assets/data/terminalData1.json');
+    return this.httpClient.get<any>('assets/data/terminalData1.json');
   }
 
   getUsers(): Observable<any> {
-    return this.http.get<any>('assets/data/userData.json');
+    return this.httpClient.get<any>('assets/data/userData.json');
   }
 
   getUsersList(): Observable<any> {
-    return this.http.get<any>('assets/data/usersList.json');
+    return this.httpClient.get<any>('assets/data/usersList.json');
   }
   
   getActionTerminals(): Observable<any>{
-    return this.http.get<any>('assets/data/actionTerminal.json');
+    return this.httpClient.get<any>('assets/data/actionTerminal.json');
   }
   getTerminal():Observable <any> {
-    return this.http.get<any>('assets/data/eachTerminal.json');
+    return this.httpClient.get<any>('assets/data/eachTerminal.json');
 
   }
   getTransactions():Observable<any>{
-    return this.http.get<any>('assets/data/transactions.json').pipe(map((res: any) => {
+    return this.httpClient.get<any>('assets/data/transactions.json').pipe(map((res: any) => {
       console.log("API response>>", res);
       return res;
     }))
