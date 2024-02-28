5a4799266cb056ef93b986d7652e9f2ac18ba087
user <user@MacBook-Pro.local>
2024-02-27 21:09:56 -0800

    added image to product

--- src/app/components/dashboard/add-product-item/add-product-item.component.ts
+++ src/app/components/dashboard/add-product-item/add-product-item.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-add-product-item',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product-item.component.scss']
 })
 export class AddProductItemComponent {
+
+
+  showModal = false;
+  productForm: FormGroup;
+  showPassword: boolean = false;
+  password: string = "password";
+  formSubmitted: boolean = false;
+  apiResponse: any;
+  countryCode: string = "";
+  countryName: string = "NIGERIA";
+
+
+  constructor(private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private toast: NgToastService
+
+  ) {
+    this.productForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      emailAddress: new FormControl('', [Validators.required, Validators.pattern('^.+@.+\..+$')]),
+      country: new FormControl(this.countryName),
+      phoneNumber: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      // optionChecked: new FormControl('', [Validators.required]),
+
+    });
+
+  }
+
+  get formData() { return this.productForm.controls; };
+
+  validateForm() {
+
+    for (let i in this.productForm.controls)
+      this.productForm.controls[i].markAsTouched();
+  }
+
+  onSubmit(){
+    
+  }
 
 }
